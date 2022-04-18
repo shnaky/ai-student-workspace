e2b69e52573e211098fb808cb817172c2383eaeb
lana <lana@levit.eu>
2022-04-18 13:22:28 +0200

    updated function description

--- 00/solution_00.py
+++ 00/solution_00.py
@@ -2,9 +2,9 @@ from module_1 import is_positive, is_even
 
 def is_even_and_positive(y):
     """
-    Please modify the function body of this function so that
-    - `y` is True if `x` is an even and positive integer
-    - `y` is `False` in all other cases
+    Please modify the body of this function so that it returns
+    - True if `y` is an even and positive integer
+    - False in all other cases
     """
     # This function body should be edited such that
     # it contains the correct solution
