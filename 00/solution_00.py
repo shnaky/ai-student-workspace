9a4a2fccc1de72c837dd25af8082bcfc3abb6bf1
Ingmar Schubert <schubertingmar@gmail.com>
2022-04-21 13:14:54 +0200

    minor

--- 00/solution_00.py
+++ 00/solution_00.py
@@ -1,9 +1,9 @@
 from module_1 import is_positive, is_even
 
-def is_even_and_positive(y):
+def is_even_and_positive(x):
     """
     Please modify the body of this function so that it returns
-    - True if `y` is an even and positive integer
+    - True if `x` is an even and positive integer
     - False in all other cases
     """
     # This function body should be edited such that
@@ -12,4 +12,4 @@ def is_even_and_positive(y):
 
     # one possibility would be to use the functions
     # provided by module_1
-    # return is_even(y) and is_positive(y)
+    # return is_even(x) and is_positive(x)
