f2e0225d42a47eb2bc7c36f41d1ea5c29dccdd06
Ingmar Schubert <schubertingmar@gmail.com>
2022-05-04 13:26:13 +0200

    Task 01

---
+++ 01/solution_01.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def maximize_mean(mu_vector, sigma_vector):
+    """
+    Please modify the body of this function according to the description in exercise 1.1.1
+    """
+    raise NotImplementedError
+
+def maximize_ucb(mu_vector, sigma_vector):
+    """
+    Please modify the body of this function according to the description in exercise 1.1.2
+    """
+    raise NotImplementedError
+
+def maximize_utility(mu_vector, sigma_vector, utility_function):
+    """
+    Please modify the body of this function according to the description in exercise 1.1.3
+    """
+    raise NotImplementedError
+
+def maximize_mean_using_simulator(simulator, A, n):
+    """
+    Please modify the body of this function according to the description in exercise 1.2
+    """
+    raise NotImplementedError
