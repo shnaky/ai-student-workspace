85e13e3b82d3501370efe1d662ae1168f7be0d4a
Ingmar Schubert <schubertingmar@gmail.com>
2022-05-18 11:50:53 +0200

    Task 02

---
+++ 02/solution_02.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def regression_based_on_data(l, u, a, y):
+    """
+    Please modify the body of this function according to the description in exercise 2.1
+    """
+    raise NotImplementedError
