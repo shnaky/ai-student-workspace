2b9644203d55a3eb69d41e8f8ec04283015e1060
Orestis Papandreou <orestis.papandreou@campus.tu-berlin.de>
2022-06-07 22:19:53 +0200

    solution_03

--- 03/solution_03.py
+++ 03/solution_03.py
@@ -1,10 +1,42 @@
 import numpy as np
 
+
 def mc_random_Q(simulator):
     """
     Please modify the body of this function according to the description in exercise 3.1
     """
-    raise NotImplementedError
+    K = simulator.getNumActions()
+    Q_values = np.zeros(K, dtype=float)
+    actions = [-1, 1]
+    n_a = np.zeros(K, dtype=int)
+
+    for n in range(K):
+        y = simulator.reset()
+        # print(simulator.state)
+        a = actions[n]
+        # print("a: {}".format(a))
+        r, y, done = simulator.step(a)
+
+        while True:
+            # print("actions.index: {}".format(actions.index(a)))
+            n_a[n] += 1
+            Q_values[n] += r
+            # print(n_a[actions.index(a)])
+
+            if done:
+                break
+
+            a = np.random.choice(actions)
+            r, y, done = simulator.step(a)
+            # print("r, y, done: {} {} {}".format(r, y, done))
+
+        # print(n_a)
+        # print(Q_values)
+    Q_values /= n_a
+    # print(n_a)
+    # print("Q_values: {}".format(Q_values))
+    return Q_values
+
 
 def mc_uct_Q(simulator):
     """
